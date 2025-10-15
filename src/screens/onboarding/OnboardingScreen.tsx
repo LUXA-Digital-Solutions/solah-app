374dce62d69ea4f3d0a7ae8f6177d2575a6a6592
Farooq Olanrewaju <ofarooqdev@gmail.com>
2025-10-15 23:21:28 +0300

    [EDIT]: Add button to move to home screen

--- src/screens/onboarding/OnboardingScreen.tsx
+++ src/screens/onboarding/OnboardingScreen.tsx
@@ -1,11 +1,19 @@
+import { router } from "expo-router";
 import { View, Text } from "react-native";
 
+import { AppButton } from "@/shared/components";
 import { emptyScreenStyle } from "@/shared/styles";
 
 export function OnboardingScreen() {
   return (
     <View style={emptyScreenStyle.container}>
       <Text>Onboarding Screen</Text>
+      <AppButton
+        title="Go to Tabs"
+        onPress={() => {
+          router.push("/(tabs)/home");
+        }}
+      />
     </View>
   );
 }
