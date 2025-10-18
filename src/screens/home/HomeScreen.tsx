0a7a22540a6984e5003aa345d5ffcf2b2d7e294d
notRadioOpaque <ismailmuyideen.c05@gmail.com>
2025-10-18 11:33:51 +0100

    feat: home screen top section

--- src/screens/home/HomeScreen.tsx
+++ src/screens/home/HomeScreen.tsx
@@ -1,8 +1,9 @@
-import { ScrollView } from "react-native";
+import { AudioLines, Bell } from "lucide-react-native";
+import { Pressable, ScrollView, Text, useWindowDimensions, View } from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 import { HomeSection as AdhkarHomeSection } from "@/features/adhkar/components/HomeSection";
-import { screenStyle } from "@/shared/styles";
+import { fontsize, screenStyle } from "@/shared/styles";
 
 /**
  * HomeScreen component
@@ -22,6 +23,8 @@ import { screenStyle } from "@/shared/styles";
 
 export function HomeScreen() {
   const { top, bottom } = useSafeAreaInsets();
+  const { width } = useWindowDimensions();
+  const CONTAINER_WIDTH = Math.min(380, width - 20);
 
   return (
     <ScrollView
@@ -31,6 +34,43 @@ export function HomeScreen() {
         paddingBottom: bottom,
       }}
     >
+      <View
+        style={{
+          flexDirection: "row",
+          width: CONTAINER_WIDTH,
+          height: 38,
+          alignItems: "center",
+          justifyContent: "space-between",
+          marginBottom: 24,
+        }}
+      >
+        <Text
+          style={{
+            fontSize: fontsize.xxxl,
+            textAlign: "left",
+            fontWeight: "bold",
+          }}
+        >
+          Prayer Guide
+        </Text>
+
+        <View
+          style={{
+            flexDirection: "row",
+            width: "auto",
+            alignItems: "center",
+            justifyContent: "space-between",
+            gap: 12,
+          }}
+        >
+          <Pressable>
+            <Bell />
+          </Pressable>
+          <Pressable>
+            <AudioLines />
+          </Pressable>
+        </View>
+      </View>
       <AdhkarHomeSection />
     </ScrollView>
   );
