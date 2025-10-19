361232a76756986f9dfad5817219204fc7c5326b
Kamaldeen30a <issakamal2443@gmail.com>
2025-10-19 11:09:04 +0100

    [FEATURE]: Add styles and icons to Bottom Tabs

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -1,20 +1,49 @@
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 import { Tabs } from "expo-router";
+import { Home, BookOpenText, Bolt } from "lucide-react-native";
 
-function Layout() {
+export function TabsLayout() {
   return (
     <Tabs
-      initialRouteName="home"
       screenOptions={{
-        headerShown: false,
-        animation: "none",
+        tabBarActiveTintColor: "#A46A3D",
+        tabBarLabelStyle: {
+          fontWeight: "bold",
+        },
       }}
     >
-      <Tabs.Screen name="home" />
-      <Tabs.Screen name="adhkar" />
-      <Tabs.Screen name="guide" />
-      <Tabs.Screen name="settings" />
+      <Tabs.Screen
+        name="home"
+        options={{
+          title: "Home",
+          tabBarIcon: ({ color, size }) => <Home color={color} size={size} />,
+        }}
+      />
+      <Tabs.Screen
+        name="adhkar"
+        options={{
+          title: "Adhkar",
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="hands-pray" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="guide"
+        options={{
+          title: "Guide",
+          tabBarIcon: ({ color, size }) => <BookOpenText color={color} size={size} />,
+        }}
+      />
+      <Tabs.Screen
+        name="settings"
+        options={{
+          title: "Settings",
+          tabBarIcon: ({ color, size }) => <Bolt color={color} size={size} />,
+        }}
+      />
     </Tabs>
   );
 }
 
-export default Layout;
+export default TabsLayout;
